4c51323d0e64ab19f7a5a4d92db56a5ddd768c8b
YuanGuan <intelinker@163.com>
2017-09-13 13:36:34 +0800

    make models & controllers

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
+            $table->string('avatar');
+            $table->string('token');
+            $table->string('uid');
+            $table->string('title');
             $table->string('password');
+            $table->integer('profile_id')->references('id')->on('profiles')->onDelete('cascade');
+            $table->integer('status_id')->references('id')->on('user_status')->onDelete('cascade');
+            $table->integer('pre_status_id')->references('id')->on('user_status')->onDelete('cascade');
+            $table->string('role');
+            $table->boolean('banned');
             $table->rememberToken();
             $table->timestamps();
         });
